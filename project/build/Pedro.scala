f4a90f61f95600257d7a4cca255cce01a6686f8f
unknown <bob@.(none)>
2010-12-18 21:26:04 -0600

    Add h2 and servlet to repo deps

--- project/build/Pedro.scala
+++ project/build/Pedro.scala
@@ -5,4 +5,6 @@ import sbt._
 class Pedro(info: ProjectInfo) extends DefaultProject(info)
 {
   val scalatest = "org.scalatest" % "scalatest" % "1.2"
+  val h2 = "com.h2database" % "h2" % "1.3.146"
+  val servletapi = "org.apache.tomcat" % "servlet-api" % "6.0.29"
 }
