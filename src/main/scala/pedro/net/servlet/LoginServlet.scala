d366116dc2cde2cda599ee4f1597396a3c6702ad
unknown <bob@.(none)>
2010-12-28 02:44:23 -0600

    new loginservlet

---
+++ src/main/scala/pedro/net/servlet/LoginServlet.scala
@@ -0,0 +1,69 @@
+/**
+ * Simple Servlet tools.
+ *
+ *
+ * Authors:
+ *   Bob Jamison
+ *
+ * Copyright (C) 2010 Bob Jamison
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+package pedro.net.servlet
+
+
+
+class LoginServlet extends Servlet
+{
+    /**
+     * Override this for your application-specific authorization
+     */         
+    def authorize(req: Request) : Option[Auth] =
+        None
+
+    override def doGet(req: Request, resp: Response) =
+        {
+        }
+
+    override def doPost(req: Request, resp: Response) =
+        {
+        val authRec = authorize(req)
+        if (authRec.isDefined)
+            {
+            
+            }
+        else
+            {
+            
+            }        
+        }
+}
+
+
+class LogoutServlet extends Servlet
+{
+    override def doGet(req: Request, resp: Response) =
+        {
+        
+        }
+
+    override def doPost(req: Request, resp: Response) =
+        {
+        
+        }
+}
+
+
