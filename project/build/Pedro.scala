eb4e61e8814f23a9e12e9b40ca9bfebab33ea96c
unknown <bjamison@.prosrm.com>
2011-01-31 07:27:59 -0600

    Add Cassandra to dependencies

--- project/build/Pedro.scala
+++ project/build/Pedro.scala
@@ -7,4 +7,5 @@ class Pedro(info: ProjectInfo) extends DefaultProject(info)
   val scalatest = "org.scalatest" % "scalatest" % "1.2"
   val h2 = "com.h2database" % "h2" % "1.3.146"
   val servletapi = "org.apache.tomcat" % "servlet-api" % "6.0.29"
+  val cassandra = "org.apache.cassandra" % "cassandra-all" % "0.7.0"
 }
