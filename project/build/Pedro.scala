a83ee735e925e1590be0cdab6ac9e594085b6f9c
unknown <bjamison@.prosrm.com>
2011-05-18 10:21:29 -0500

    Made deps intransitive

--- project/build/Pedro.scala
+++ project/build/Pedro.scala
@@ -7,8 +7,8 @@ import sbt._
  */
 class Pedro(info: ProjectInfo) extends DefaultProject(info)
 {
-  val scalatest  = "org.scalatest"        % "scalatest_2.9.0" % "1.4.1"   % "compile,test"
-  val h2         = "com.h2database"       % "h2"              % "1.3.154" % "compile,test"
-  val servletapi = "org.apache.tomcat"    % "servlet-api"     % "6.0.29"  % "compile,test"
-  val cassandra  = "org.apache.cassandra" % "cassandra-all"   % "0.7.5"   % "compile,test"
+  val scalatest  = "org.scalatest"        % "scalatest_2.9.0" % "1.4.1"   % "compile,test" intransitive()
+  val h2         = "com.h2database"       % "h2"              % "1.3.154" % "compile,test" intransitive()
+  val servletapi = "org.apache.tomcat"    % "servlet-api"     % "6.0.29"  % "compile,test" intransitive()
+  val cassandra  = "org.apache.cassandra" % "cassandra-all"   % "0.7.5"   % "compile,test" intransitive()
 }
