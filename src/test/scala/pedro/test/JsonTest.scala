44aad1a36dbd82b28299a3bae8889b1c48e8d64b
Bob Jamison <ishmalius@gmail.com>
2011-06-17 10:04:35 -0500

    new test for json

---
+++ src/test/scala/pedro/test/JsonTest.scala
@@ -0,0 +1,29 @@
+
+
+
+package pedro.data
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.matchers.MustMatchers
+import org.scalatest.{FeatureSpec,GivenWhenThen}
+
+import pedro.data.{Json,JsonParser}
+
+
+
+@RunWith(classOf[JUnitRunner])
+class JsonTest extends FeatureSpec with GivenWhenThen with MustMatchers
+{
+    feature("JSON serialization and deserialization")
+        {
+        scenario("JSON should be able to parse this string")
+            {
+            }
+
+    
+        }
+
+}
+
+
